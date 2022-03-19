be907615095dfb97440b3b50b8e10e24d5d37155
Jeswin Santosh <jeswin.santosh@outlook.com>
2022-03-19 19:11:46 +0530

    adding main files

---
+++ js/script.js
@@ -0,0 +1,165 @@
+const X_CLASS = 'X'
+const O_CLASS = 'O'
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let OsTurn
+
+let ai = sessionStorage.getItem("first"), kind = sessionStorage.getItem("second")
+
+function start() {
+  if(ai == 'true') {
+    startGame_CvP()
+  } else {
+    startGame_PvP()
+  }
+}
+
+start()
+
+restartButton.addEventListener('click', start)
+
+function startGame_CvP() {
+  OsTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(O_CLASS)
+    cell.removeEventListener('click', handleClick2)
+    cell.addEventListener('click', handleClick2, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+function startGame_PvP() {
+  OsTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(O_CLASS)
+    cell.removeEventListener('click', handleClick1)
+    cell.addEventListener('click', handleClick1, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+function handleClick1(e) {
+  const cell = e.target
+  const currentClass = OsTurn ? O_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+function handleClick2(e) {
+  const currentClass = OsTurn ? O_CLASS : X_CLASS
+  if(!OsTurn)
+    placeMark(e.target, X_CLASS)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else {
+    swapTurns()
+    let place = false, skip = false, i = 0
+    cellElements.forEach(cell => {
+      if(skip)
+        return
+      if(!cell.classList.contains(X_CLASS) && !cell.classList.contains(O_CLASS)) {
+        placeMark(cell, O_CLASS)
+        if(checkWin(O_CLASS))
+          endGame(false)
+        else {
+          if(kind == '3x3' ? i == 4 : ([5, 6, 9, 10].includes(i))) {
+            place = true
+            skip = true
+            return
+          }
+          cell.classList.remove(O_CLASS)
+          placeMark(cell, X_CLASS)
+          if(checkWin(X_CLASS)) {
+            place = true
+            cell.classList.remove(X_CLASS)
+            placeMark(cell, O_CLASS)
+            cell.removeEventListener('click', handleClick2)
+            skip = true
+            return
+          }
+          cell.classList.remove(X_CLASS)
+        }
+      }
+      i++
+    })
+    if(!place) {
+      skip = false
+      place = false
+      i = 0
+      arr1 = kind == '3x3' ? [0, 2, 4, 6, 8] : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+      cellElements.forEach(cell => {
+        if(skip)
+          return
+        if(arr1.includes(i)  && (!cell.classList.contains(X_CLASS) && !cell.classList.contains(O_CLASS))) {
+          place = true
+          placeMark(cell, O_CLASS)
+          cell.removeEventListener('click', handleClick2)
+          skip = true
+        }
+        i++
+      })
+      if(!place) {
+
+      }
+    }
+    if (isDraw()) {
+      endGame(true)
+    }
+    swapTurns()
+  }   
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = ai == 'true' ? `${OsTurn ? "GAME OVER" : "You Win!"}` : `${OsTurn ? "O" : "X"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  OsTurn = !OsTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(O_CLASS)
+  if (OsTurn) {
+    board.classList.add(O_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
---
+++ js/script_main.js
@@ -0,0 +1,33 @@
+const tile1 = document.getElementById('t1')
+const tile2 = document.getElementById('t2')
+let ai
+
+tile1.addEventListener('click', function select() {
+    handleClick('3x3')
+})
+tile2.addEventListener('click', function select() {
+    handleClick('4x4')
+})
+
+function handleClick(kind) {
+    tile1.innerHTML = 'vs Human'
+    tile2.innerHTML = 'vs Computer'
+    tile1.style.backgroundColor = 'black'
+    tile2.style.backgroundColor = 'black'
+    tile1.style.borderRadius = '5%'
+    tile2.style.borderRadius = '5%'
+    tile1.style.fontSize = '35px'
+    tile2.style.fontSize = '35px'
+    tile1.addEventListener('click', function type() {
+        ai = false
+        sessionStorage.setItem("first", ai);
+        sessionStorage.setItem("second", kind);
+        location.href = kind + '.html'
+    })
+    tile2.addEventListener('click', function type() {
+        ai = true
+        sessionStorage.setItem("first", ai);
+        sessionStorage.setItem("second", kind);
+        location.href = kind + '.html'
+    })    
+}
